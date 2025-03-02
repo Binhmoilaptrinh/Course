ebfa81bc80a093cc4d6c9fbf3562b0122335e3d6
BinhPTHE171223 <BinhPTHE171223@fpt.edu.vn>
2025-03-02 09:46:59 +0700

    update-newdb

--- CourseManagement/WebAPI/Extensions/ServiceExtensions.cs
+++ CourseManagement/WebAPI/Extensions/ServiceExtensions.cs
@@ -2,11 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using WebAPI.Data;
 using System.Text;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using WebAPI.Services.Interfaces;
 using WebAPI.Services;
+using WebAPI.Models;
 
 namespace WebAPI.Extensions
 {
@@ -14,65 +14,10 @@ namespace WebAPI.Extensions
     {
         public static void ConfigureServiceManager(this IServiceCollection services)
         {
-            services.AddScoped<IAuthenticateService, AuthenticateService>();
             services.AddHttpContextAccessor();
         }
-
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
-        services.AddDbContext<ElearningCourseContext>(opts =>
-            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
-
-        public static void ConfigureIdentity(this IServiceCollection services)
-        {
-            var builder = services.AddIdentity<AspNetUser, IdentityRole>(o =>
-            {
-                o.Password.RequireDigit = true;
-                o.Password.RequireLowercase = false;
-                o.Password.RequireUppercase = false;
-                o.Password.RequireNonAlphanumeric = false;
-                o.Password.RequiredLength = 10;
-                o.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<ElearningCourseContext>()
-            .AddDefaultTokenProviders();
-        }
-
-        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
-        {
-            var jwtSettings = configuration.GetSection("JwtSettings");
-            var secretKey = jwtSettings["secretKey"];
-
-            services.AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var token = context.Request.Cookies["jwtToken"];
-                        if (!string.IsNullOrEmpty(token))
-                        {
-                            context.Token = token;
-                        }
-                        return Task.CompletedTask;
-                    }
-                };
-
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings["validIssuer"],
-                    ValidAudience = jwtSettings["validAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
-                };
-            });
-        }
+        services.AddDbContext<ECourseContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
     }
 }
