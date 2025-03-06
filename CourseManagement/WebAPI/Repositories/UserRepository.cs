45a9dd7f20c6aef6d5d6d26fa353509e22ddb9cd
Binhmoilaptrinh <131687077+Binhmoilaptrinh@users.noreply.github.com>
2025-03-07 00:09:09 +0700

    Update UserRepository.cs

--- CourseManagement/WebAPI/Repositories/UserRepository.cs
+++ CourseManagement/WebAPI/Repositories/UserRepository.cs
@@ -1,85 +1,63 @@
-<<<<<<< HEAD
 ﻿using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
-=======
-﻿using WebAPI.Models;
->>>>>>> 87b1073cfd82b875b0b5b5b2d6f1d83de6a1a9f6
 using WebAPI.Repositories.Interfaces;
 
 namespace WebAPI.Repositories
 {
     public class UserRepository : IUserRepository
     {
-<<<<<<< HEAD
-        private readonly ECourseContext _courseContext;
+        private readonly ECourseContext _context;
 
-        public UserRepository(ECourseContext courseContext)
+        public UserRepository(ECourseContext context)
         {
-            _courseContext = courseContext;
+            _context = context;
         }
 
         public async Task<User> CreateAsync(User user)
         {
-            _courseContext.Users.Add(user);
-            await _courseContext.SaveChangesAsync();
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
             return user;
         }
 
         public async Task<User> UpdateAsync(User user)
         {
-            _courseContext.Entry(user).State = EntityState.Modified;
-            await _courseContext.SaveChangesAsync();
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
             return user;
         }
 
         public async Task<bool> CheckAsyncForUserName(string username)
         {
-            var check = _courseContext.Users.Any(e => e.Username == username);
-            await _courseContext.SaveChangesAsync();
-            return check;
-
+            return await _context.Users.AnyAsync(e => e.Username == username);
         }
+
         public async Task<bool> CheckAsyncForEmail(string email)
         {
-            var check = _courseContext.Users.Any(e => e.Email == email);
-            await _courseContext.SaveChangesAsync();
-            return check;
-
+            return await _context.Users.AnyAsync(e => e.Email == email);
         }
-        
+
         public async Task<User> GetUserAsyncForUserName(string userName)
         {
-            return await _courseContext.Users.FirstOrDefaultAsync(u => u.Username == userName);
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == userName);
         }
 
         public async Task<User> GetUserAsyncForEmail(string email)
         {
-            return await _courseContext.Users.FirstOrDefaultAsync(u => u.Email == email);
-=======
-        private readonly ECourseContext _context;
-
-
-        public UserRepository(ECourseContext context)
-        {
-            _context = context;
->>>>>>> 87b1073cfd82b875b0b5b5b2d6f1d83de6a1a9f6
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
 
         public async Task<User> GetAsync(int id)
         {
-<<<<<<< HEAD
-            return await _courseContext.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
         }
 
         public async Task<List<UserRole>> GetAllAsync()
         {
-            return await _courseContext.UserRoles
+            return await _context.UserRoles
                 .Include(ur => ur.Role)  // Lấy thông tin từ bảng Role
                 .Include(ur => ur.User)  // Lấy thông tin từ bảng User
                 .ToListAsync();
-=======
-            return await _context.Users.FindAsync(id);
->>>>>>> 87b1073cfd82b875b0b5b5b2d6f1d83de6a1a9f6
         }
     }
 }
