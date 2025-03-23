299cf721024c1bcd656e8ab3853cccee611e37b1
cuongtran6969 <cuongtvhe176258@fpt.edu.vn>
2025-03-24 01:02:21 +0700

    Update course + manage chapter

--- CourseManagement/WebAPI/DTOS/response/CourseDetailAdmin.cs
+++ CourseManagement/WebAPI/DTOS/response/CourseDetailAdmin.cs
@@ -3,13 +3,19 @@
     public class CourseDetailAdmin
     {
         public string Thumbnail {  get; set; }
+
+        public string PreviewVideo { get; set; }
         public int Enrollments { get; set; }
         public int CourseId { get; set; }   
         public string Title { get; set; }
-        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
         public int LessonsCount { get; set; }
         public double Price { get; set; }
         public string Status { get; set; }
+        
+        public string Description { get; set; }
+
+        public int? LimitDay { get; set; }
 
     }
 }
