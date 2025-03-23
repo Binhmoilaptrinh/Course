24e589787236595eda815f541a7fd46c16f1b3bd
BinhPTHE171223 <BinhPTHE171223@fpt.edu.vn>
2025-03-23 10:48:25 +0700

    push-avatar

--- CourseManagement/WebAPI/DTOS/response/LoginResponseDto.cs
+++ CourseManagement/WebAPI/DTOS/response/LoginResponseDto.cs
@@ -13,6 +13,9 @@ namespace WebAPI.DTOS.response
         public int RoleId { get; set; }
 
         public string? RoleName { get; set; }
+        public string? Avatar { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Bio { get; set; }
 
         public bool Success { get; set; }  // ✅ Thêm thuộc tính Success
         public string? Token { get; set; }
