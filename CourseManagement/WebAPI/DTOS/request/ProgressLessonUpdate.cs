c4ece8eb5462760220d7be8d7fdfcf9e2c25c20c
BinhPTHE171223 <BinhPTHE171223@fpt.edu.vn>
2025-03-26 16:01:01 +0700

    add-them

--- CourseManagement/WebAPI/DTOS/request/ProgressLessonUpdate.cs
+++ CourseManagement/WebAPI/DTOS/request/ProgressLessonUpdate.cs
@@ -5,6 +5,5 @@
         public int LessonId { get; set; }
         public int UserId { get; set; }
         public float ProgressPercentage { get; set; }
-        public int Passing {  get; set; }
     }
 }
