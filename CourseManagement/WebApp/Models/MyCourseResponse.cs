7b7673136a3dbe96e4212d8c9814378474924c71
BinhPTHE171223 <BinhPTHE171223@fpt.edu.vn>
2025-03-28 00:24:53 +0700

    fix

--- CourseManagement/WebApp/Models/MyCourseResponse.cs
+++ CourseManagement/WebApp/Models/MyCourseResponse.cs
@@ -5,6 +5,7 @@ namespace WebApp.Models
     public class MyCourseResponse
     {
         public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
         public string? UserName { get; set; }
         public string? Title { get; set; }
         public string? Category { get; set; }
